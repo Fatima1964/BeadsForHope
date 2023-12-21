0a41debec797b8e7184154b8cdbccba99f5f00be
Fatima1964 <143363325+Fatima1964@users.noreply.github.com>
2023-12-21 16:08:00 -0700

    Edited content

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -10,13 +10,6 @@ const Mission = () => {
         My goal is to contribute to various charitable causes and make a difference
         in the lives of those in need.
       </p>
-      <p>A percentage of my profits goes towards charitable organizations
-        working towards a better and brighter future for everyone.
-      </p>
-      <p>
-        Together, we can create a world where kindness and compassion prevail.
-        Thank you for being a part of my mission to make the world a better place!
-      </p>
     </div>
   );
 };
